8001cf9123eb0c88efd1bfe8d8ffb0dc42354902
Vladimir Alekseichenko <vova@vova.me>
2013-09-03 06:46:08 +0200

    add infinite sequence

--- haskell/Factorial.hs
+++ haskell/Factorial.hs
@@ -14,6 +14,7 @@ fact1 n | n == 0 = 1
                        | k > 1  = fact1' (k-1) (v*k)
 
 fact2 n = product[1..n]
-
 fact3 n = foldr (*) 1 [1..n]
 
+{- Infinite sequence -}
+fact4 = scanl1 (*) [1..]
